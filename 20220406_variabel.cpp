cfb93c7dd672d786d22e6a3be967c83ea8b794ee
Yasinta1010 <103085975+Yasinta1010@users.noreply.github.com>
2022-04-05 20:08:50 -0700

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+using namespace std;
+int main(){//begin of function
+float angka;
+string nama; //deklarasi variabel
+nama = "Yasinta Sieman"; //masukkan value kedalam variabel
+angka = 10.21 + 3.14;
+cout<<nama;//menampilkan variabel
+cout<<nama<<nama<<nama<<endl; //menampilkan variabel
+cout<<angka<<endl;
+}//end of function
