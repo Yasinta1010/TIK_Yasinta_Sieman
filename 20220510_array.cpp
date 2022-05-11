aade659d63427f1b8cd05430998e247f66292d1c
Yasinta1010 <103085975+Yasinta1010@users.noreply.github.com>
2022-05-10 19:02:23 -0700

    Create 20220510_array.cpp

---
+++ 20220510_array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama [7] = {
+        "David", 
+        "Kabila", 
+        "Bryan", 
+        "Sidney",
+        "Gracia",
+        "Keysie",
+        "Yoel",
+    };
+    for (int a = 0; a<7 ; a ++){
+        cout<<nama[a]<<endl;
+    }
+    return 0;
+}
